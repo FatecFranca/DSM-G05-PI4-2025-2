68b77a01b3ac428f38a903b7953881f736a8ed66
schizary <gschizari@gmail.com>
2025-11-27 00:19:39 -0300

    feat(web): alinhando graficos com o mobile

--- web/src/components/charts/LineChart.js
+++ web/src/components/charts/LineChart.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
-const CustomLineChart = ({ data, dataKey, xAxisDataKey, color = '#656CEE' }) => {
+const CustomLineChart = ({ data, dataKey, xAxisDataKey, color = '#656CEE', angle = 0 }) => {
   return (
     <ResponsiveContainer width="100%" height={300}>
       <LineChart data={data}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey={xAxisDataKey} />
+        <XAxis 
+          dataKey={xAxisDataKey} 
+          angle={angle}
+          textAnchor={angle !== 0 ? 'end' : 'middle'}
+          height={angle !== 0 ? 60 : 30}
+        />
         <YAxis />
         <Tooltip />
-        <Line type="monotone" dataKey={dataKey} stroke={color} />
+        <Line type="monotone" dataKey={dataKey} stroke={color} strokeWidth={2} dot={{ r: 4 }} />
       </LineChart>
     </ResponsiveContainer>
   );
